2f8f9ded0b8e674d74eeb799be0fdff151baf623
Lucas Medeiros <medeirosrosalucas@gmail.com>
2021-11-02 13:29:33 -0300

    Adicionar política de CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from app.views import server_bp
 from settings import APP_SETTINGS
+from flask_cors import CORS
 
 
 def create_app(settings=None):
     app = Flask(__name__)
+
+    CORS(app)
+    app.config["CORS_HEADERS"] = "Content-Type"
 
     if settings:
         app.config.from_object(settings)
